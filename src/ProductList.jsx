5f35ec73bebd25b24d0766f8c1abf66ce95728e8
babaluis <luisbaba62@gmail.com>
2025-09-28 23:04:31 +0100

    final steps

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -272,6 +272,7 @@ function ProductList({ onHomeClick }) {
         [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
     }));
     };
+   
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -292,13 +293,13 @@ function ProductList({ onHomeClick }) {
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>{totalItems > 0 && (
         <span style={{
           position: "absolute",
-          top: "10px",
-          right: "10px",
+          top: "35px",
+          right: "40px",
           background: "red",
           color: "white",
           borderRadius: "50%",
           padding: "4px 8px",
-          fontSize: "14px"
+          fontSize: "18px"
         }}>
           {totalItems}
         </span>
@@ -309,8 +310,8 @@ function ProductList({ onHomeClick }) {
                 <div className="product-grid">
                     {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
                     <div key={index}> {/* Unique key for each category div */}
-                        <h1>
-                        <div>{category.category}</div> {/* Display the category name */}
+                        <h1 >
+                        <div className="product-list">{category.category}</div> {/* Display the category name */}
                         </h1>
                         <div className="product-list"> {/* Container for the list of plant cards */}
                         {category.plants.map((plant, plantIndex) => ( // Loop through each plant in the current category
